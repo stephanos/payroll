a2a6b7e6db317160d67ba88ed5b7008f1f140f90
Stephan Behnke <git@stephanbehnke.de>
2017-04-29 19:27:59 -0400

    init dev setup

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as Koa from 'koa';
+import { Request } from 'koa';
+import * as Router from 'koa-router';
+
+
+const router = new Router();
+router.get('/', async (ctx, next) => {
+    await next();
+    ctx.body = 'Hello!';
+    ctx.status = 200;
+});
+
+
+const app = new Koa();
+app.use(router.routes());
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+export { app };
