e53aa88630c291d7a37bc0d4c58bd0495c28fa82
Stephan Behnke <git@stephanbehnke.de>
2017-04-30 08:36:17 -0400

    add ts-node, remove extra build step

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("reflect-metadata");
-var http_1 = require("./http");
-process.on('uncaughtException', function (err) {
-    console.log('Caught exception: ' + err); // tslint:disable-line:no-console
-});
-http_1.startServer();
-//# sourceMappingURL=index.js.map
